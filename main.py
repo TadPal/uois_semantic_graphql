203dafbee9c55e3ed6c5fe57ec55323ae78d26b9
JanosNovas <jan.novotny2002@gmail.com>
2025-09-01 14:06:41 +0200

    barvy emoty

--- main.py
+++ main.py
@@ -152,40 +152,67 @@ async def index_page(request: Request):
             if feedback_row:
                 feedback_row.delete()
             with ui.row().classes("ml-12 gap-1 -mt-4") as feedback_row:
-                # Like (palec nahoru)
-                with ui.button(on_click=lambda: print("Like clicked")).props("flat dense").classes("p-1"):
-                    ui.html("""
-                    <svg class="w-6 h-6 text-green-600 dark:text-green-400" xmlns="http://www.w3.org/2000/svg" 
-                        fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" 
-                            d="M7 11c.889-.086 1.416-.543 2.156-1.057a22.323 22.323 0 0 0 
-                                3.958-5.084 1.6 1.6 0 0 1 .582-.628 1.549 1.549 0 0 1 
-                                1.466-.087c.205.095.388.233.537.406a1.64 1.64 0 0 1 
-                                .384 1.279l-1.388 4.114M7 11H4v6.5A1.5 1.5 0 0 0 
-                                5.5 19v0A1.5 1.5 0 0 0 7 17.5V11Zm6.5-1h4.915c.286 0 
-                                .372.014.626.15.254.135.472.332.637.572a1.874 1.874 
-                                0 0 1 .215 1.673l-2.098 6.4C17.538 19.52 17.368 20 
-                                16.12 20c-2.303 0-4.79-.943-6.67-1.475"/>
-                    </svg>
-                    """)
+                # --- LIKE ---
+                like_default = """
+                <svg class="w-6 h-6 text-blue-700 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" 
+                        d="M7 11c.889-.086 1.416-.543 2.156-1.057a22.323 22.323 0 0 0 
+                            3.958-5.084 1.6 1.6 0 0 1 .582-.628 1.549 1.549 0 0 1 
+                            1.466-.087c.205.095.388.233.537.406a1.64 1.64 0 0 1 
+                            .384 1.279l-1.388 4.114M7 11H4v6.5A1.5 1.5 0 0 0 
+                            5.5 19v0A1.5 1.5 0 0 0 7 17.5V11Zm6.5-1h4.915c.286 0 
+                            .372.014.626.15.254.135.472.332.637.572a1.874 1.874 
+                            0 0 1 .215 1.673l-2.098 6.4C17.538 19.52 17.368 20 
+                            16.12 20c-2.303 0-4.79-.943-6.67-1.475"/>
+                </svg>
+                """
 
-                # Dislike (palec dolů)
-                with ui.button(on_click=lambda: print("Dislike clicked")).props("flat dense").classes("p-1"):
-                    ui.html("""
-                    <svg class="w-6 h-6 text-red-600 dark:text-red-400" xmlns="http://www.w3.org/2000/svg" 
-                        fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" 
-                            d="M17 13c-.889.086-1.416.543-2.156 1.057a22.322 
-                                22.322 0 0 0-3.958 5.084 1.6 1.6 0 0 1-.582.628 
-                                1.549 1.549 0 0 1-1.466.087 1.587 1.587 0 0 1 
-                                -.537-.406 1.666 1.666 0 0 1-.384-1.279l1.389-4.114
-                                M17 13h3V6.5A1.5 1.5 0 0 0 18.5 5v0A1.5 1.5 0 0 0 
-                                17 6.5V13Zm-6.5 1H5.585c-.286 0-.372-.014-.626-.15
-                                a1.797 1.797 0 0 1-.637-.572 1.873 1.873 0 0 1 
-                                -.215-1.673l2.098-6.4C6.462 4.48 6.632 4 7.88 4c2.302 
-                                0 4.79.943 6.67 1.475"/>
-                    </svg>
-                    """)
+                like_selected = """
+                <svg class="w-6 h-6 text-blue-700 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" 
+                    width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M15.03 9.684h3.965c.322 0 .64.08.925.232.286.153.532.374.717.645a2.109 2.109 0 0 1 .242 1.883l-2.36 7.201c-.288.814-.48 1.355-1.884 1.355-2.072 0-4.276-.677-6.157-1.256-.472-.145-.924-.284-1.348-.404h-.115V9.478a25.485 25.485 0 0 0 4.238-5.514 1.8 1.8 0 0 1 .901-.83 1.74 1.74 0 0 1 1.21-.048c.396.13.736.397.96.757.225.36.32.788.269 1.211l-1.562 4.63ZM4.177 10H7v8a2 2 0 1 1-4 0v-6.823C3 10.527 3.527 10 4.176 10Z" clip-rule="evenodd"/>
+                </svg>
+                """
+
+                like_icon = ui.html(like_default).classes("cursor-pointer")
+
+                def on_like_click():
+                    like_icon.set_content(like_selected)
+
+                like_icon.on('click', on_like_click)
+
+                # --- DISLIKE ---
+                dislike_default = """
+                <svg class="w-6 h-6 text-blue-400 dark:text-gray-400" xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                    <path stroke-linecap="round" stroke-linejoin="round" 
+                        d="M17 13c-.889.086-1.416.543-2.156 1.057a22.322 
+                            22.322 0 0 0-3.958 5.084 1.6 1.6 0 0 1-.582.628 
+                            1.549 1.549 0 0 1-1.466.087 1.587 1.587 0 0 1 
+                            -.537-.406 1.666 1.666 0 0 1-.384-1.279l1.389-4.114
+                            M17 13h3V6.5A1.5 1.5 0 0 0 18.5 5v0A1.5 1.5 0 0 0 
+                            17 6.5V13Zm-6.5 1H5.585c-.286 0-.372-.014-.626-.15
+                            a1.797 1.797 0 0 1-.637-.572 1.873 1.873 0 0 1 
+                            -.215-1.673l2.098-6.4C6.462 4.48 6.632 4 7.88 4c2.302 
+                            0 4.79.943 6.67 1.475"/>
+                </svg>
+                """
+
+                dislike_selected = """
+                <svg class="w-6 h-6 text-blue-400 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" 
+                    width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M8.97 14.316H5.004c-.322 0-.64-.08-.925-.232a2.022 2.022 0 0 1-.717-.645 2.108 2.108 0 0 1-.242-1.883l2.36-7.201C5.769 3.54 5.96 3 7.365 3c2.072 0 4.276.678 6.156 1.256.473.145.925.284 1.35.404h.114v9.862a25.485 25.485 0 0 0-4.238 5.514c-.197.376-.516.67-.901.83a1.74 1.74 0 0 1-1.21.048 1.79 1.79 0 0 1-.96-.757 1.867 1.867 0 0 1-.269-1.211l1.562-4.63ZM19.822 14H17V6a2 2 0 1 1 4 0v6.823c0 .65-.527 1.177-1.177 1.177Z" clip-rule="evenodd"/>
+                </svg>
+                """
+
+                dislike_icon = ui.html(dislike_default).classes("cursor-pointer")
+
+                def on_dislike_click():
+                    dislike_icon.set_content(dislike_selected)
+
+                dislike_icon.on('click', on_dislike_click)
+
 
 
         ui.run_javascript("window.scrollTo(0, document.body.scrollHeight)")
